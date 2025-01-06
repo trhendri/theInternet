69dbb122aa307430fab0b177ec842a211434772a
Talise Hendrix <trhendri1@gmail.com>
2025-01-05 23:45:18 -0600

    Add New theinternet2.js and Organize

--- page.js
+++ page.js
@@ -1,35 +1,38 @@
 module.exports = {
-  //Inputs
-  flashAlert: ".success",
-  usernameField: "#username",
-  passwordField: "#password",
+    //Inputs
+    flashAlert: ".success",
+    usernameField: "#username",
+    passwordField: "#password",
 
-  //Buttons
-  loginButton: 'button[type="submit"]',
-  addElementButton: "button=Add Element",
-  deleteButton: ".added-manually",
+    //Buttons
+    loginButton: 'button[type="submit"]',
+    addElementButton: "button=Add Element",
+    deleteButton: ".added-manually",
 
-  //Functions
+    //Functions
 
-  loginApp: async function (username, password) {
-    await browser.url("/login");
-    const usernameField = await $(this.usernameField);
-    await usernameField.setValue("tomsmith");
-    const passwordField = await $(this.passwordField);
-    await passwordField.setValue("SuperSecretPassword!");
-    const loginButton = await $(this.loginButton);
-    await loginButton.click();
-  },
+    loginApp: async function (username, password) {
+        await browser.url("/login");
+        const usernameField = await $(this.usernameField);
+        await usernameField.setValue("tomsmith");
+        const passwordField = await $(this.passwordField);
+        await passwordField.setValue("SuperSecretPassword!");
+        const loginButton = await $(this.loginButton);
+        await loginButton.click();
+    },
 
-  addElement: async function () {
-    await browser.url("/add_remove_elements/");
-    const addElementButton = await $(this.addElementButton);
-    await addElementButton.click();
-  },
+    addElement: async function () {
+        await browser.url("/add_remove_elements/");
+        const addElementButton = await $(this.addElementButton);
+        await addElementButton.click();
+    },
 
-  deleteElement: async function () {
-    await $(this.deleteButton).click();
-  },
+    deleteElement: async function () {
+        await $(this.deleteButton).click();
+    },
 
-  //logoutPage:
+    test1: 4
+
+    //logoutPage:
 };
+
---
+++ test/specs/theinternet2.js
@@ -0,0 +1,18 @@
+const page = require("../../page");
+//test
+describe('Login Page Tests', () =>{
+    it('Should verify login with valid credentials', async () => {
+       
+    });
+    it('Should verify login with invalid credentials', async () => {
+       
+    });
+});
+
+describe('dakfjahkdf', () => {
+    it('adfljadfa', async() =>{
+
+
+    });
+
+});
